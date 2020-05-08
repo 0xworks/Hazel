5788a72c43cc7547e8a4fbd03f13c9aa253bc633
freeman40 <jrfwaite@gmail.com>
2020-05-08 22:16:10 +1200

    Increase number of entities for "stress" test

--- Hazel/src/Hazel/ECS/ECSType.h
+++ Hazel/src/Hazel/ECS/ECSType.h
@@ -7,7 +7,7 @@ namespace Hazel {
 	namespace ECS {
 
 		using Entity = uint32_t;
-		const Entity MAX_ENTITIES = 5000;
+		const Entity MAX_ENTITIES = 15000;
 		using ComponentType = uint8_t;
 		const ComponentType MAX_COMPONENTS = 32;
 		using Signature = std::bitset<MAX_COMPONENTS>;
