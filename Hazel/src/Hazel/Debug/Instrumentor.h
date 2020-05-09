a80bd3f30885f805fd382b15106f267b9b7a6f26
freeman40 <jrfwaite@gmail.com>
2020-05-09 18:48:06 +1200

    Add "FrameMark" for profiling

--- Hazel/src/Hazel/Debug/Instrumentor.h
+++ Hazel/src/Hazel/Debug/Instrumentor.h
@@ -3,15 +3,16 @@
 #include "Tracy/Tracy.hpp"
 
 
-#define HZ_PROFILE 1
 #if HZ_PROFILE
 #define HZ_PROFILE_BEGIN_SESSION(name, filepath)
 #define HZ_PROFILE_END_SESSION()
 #define HZ_PROFILE_SCOPE(name) ZoneScopedN(name)
 #define HZ_PROFILE_FUNCTION() ZoneScoped
+#define HZ_PROFILE_FRAMEMARKER() FrameMark
 #else
 #define HZ_PROFILE_BEGIN_SESSION(name, filepath)
 #define HZ_PROFILE_END_SESSION()
 #define HZ_PROFILE_SCOPE(name)
 #define HZ_PROFILE_FUNCTION()
+#define HZ_PROFILE_FRAMEMARKER
 #endif
